f2421c7dbc459d1aeaf72c22e59a34c5508a35cd
khwoo <yujx7958145>
2022-04-24 19:54:57 +0900

    foreach

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Script from "next/script";
 import Layout, {siteTitle} from "../components/layout/layout";
 import layoutStyle from '../styles/Layout.module.scss'
-export default function FirstPost() {
+export default function FirstPost({num, data, datastr}: any) {
     return (
         <>
             <Layout>
@@ -24,7 +24,45 @@ export default function FirstPost() {
                         <a>back go Home</a>
                     </Link>
                 </h2>
+                <div>
+                    <div>Num: {num}, data Count: {data.length}</div>
+                    <div>
+                        {
+                            data.map((item: Info) => (
+                                <div key={item.value}>name: {item.name}, value : {item.value}</div>
+                            ))
+                        }
+                    </div>
+                </div>
             </Layout>
         </>
     )
+}
+
+interface Info {
+    name: string;
+    value: string;
+    age: number;
+}
+
+export async function getServerSideProps() {
+    let data: Array<Info> = new Array<Info>()
+    data.push({ name: 'color', value: 'red', age: 26 })
+    data.push({ name: 'color', value: 'blue', age: 26 })
+
+    let datalist = data.map((item: Info) => {
+        return {
+            name: item.name,
+            value: item.value
+        }
+    })
+    let datastr: string[] = ['1', '2', '3']
+    const num: number = 10
+    return {
+        props: {
+            data: datalist,
+            num,
+            datastr
+        }
+    }
 }
