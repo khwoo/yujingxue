bc1d00195d6aba79c127eff064f0bcbfd50fd521
khwoo <yujx1100@gmail.com>
2022-05-28 17:56:12 +0900

    2

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <IndexHeader>
                 <div className={indexStyle.menu}>
                     <span>스토어1</span>
-                    <span>상품</span>
+                    <span>상품2</span>
                     <span>투어패스</span>
                     <span>여행정보</span>
                     <span>이벤트/공지사항</span>
